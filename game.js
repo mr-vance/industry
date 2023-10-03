710e2916f4484476592021cfe13e7cbcbf7dbc79
vance <muchongo.vance@gmail.com>
2023-10-03 12:44:48 +0200

    center the screen

--- game.js
+++ game.js
@@ -4,10 +4,13 @@ var config = {
     height: 600,
     backgroundColor: '#ffffff',
     scene: [mainPage, levelOne, levelTwo, levelThree, gameEnd, levelOneOver, levelTwoOver, levelThreeOver, gameCredits],
-    pixelArt: true
-}
-  
+    pixelArt: true,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY, // Center horizontally
+    }
+};
 
 window.onload = function(){
     var game = new Phaser.Game(config);
-}
+};
